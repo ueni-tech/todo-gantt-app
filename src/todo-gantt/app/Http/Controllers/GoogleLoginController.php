f0c92e327dc862066d41758a1c8393d240ef5bee
uenishi <ueni.tech@gmail.com>
2024-04-21 10:55:52 +0900

    #1 socialite コントローラー作成

---
+++ src/todo-gantt/app/Http/Controllers/GoogleLoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleLoginController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $socialiteUser = Socialite::driver('google')->user();
+            $email = $socialiteUser->email;
+
+            $user = User::updateOrCreate([
+                'provider_id' => $socialiteUser->id,
+                'provider' => 'google',
+            ], [
+                'name' => $socialiteUser->name,
+                'email' => $email
+            ]);
+
+            Auth::login($user);
+
+            return redirect('/dashboard');
+        } catch (Exception $e) {
+            Log::error($e);
+            throw $e;
+        }
+    }
+}
