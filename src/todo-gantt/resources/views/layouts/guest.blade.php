c6be02c40ac10965fe8c0a7ce6881cd729e5fd6e
uenishi <ueni.tech@gmail.com>
2024-04-21 16:10:26 +0900

    #1 googleでログインボタン　デザイン変更

--- src/todo-gantt/resources/views/layouts/guest.blade.php
+++ src/todo-gantt/resources/views/layouts/guest.blade.php
@@ -13,6 +13,10 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
