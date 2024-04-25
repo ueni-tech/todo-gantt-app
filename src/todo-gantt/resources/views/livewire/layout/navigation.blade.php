58bcdb3c583c911658e9f480852a51ff4fc0a9da
uenishi <ueni.tech@gmail.com>
2024-04-25 22:38:14 +0900

    #3 作業：ヘッダー

--- src/todo-gantt/resources/views/livewire/layout/navigation.blade.php
+++ src/todo-gantt/resources/views/livewire/layout/navigation.blade.php
@@ -18,22 +18,22 @@ new class extends Component
 
 <nav x-data="{ open: false }" class="bg-primary-500 border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" wire:navigate>
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="/" wire:navigate>
+                        <h1 class="text-lg font-bold tracking-wide text-emerald-900 bg-neutral-100 py-1 px-2">Todo-Gannt</h1>
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex sm:items-center">
+                    <x-nav-link class="text-neutral-100" :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         your Todos
                     </x-nav-link>
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
+                    <x-nav-link class="text-neutral-100" :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         team Ganttcharts
                     </x-nav-link>
                 </div>
