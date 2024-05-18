463356eb5262716d9fd41677078d03f02aee6cac
uenishi <ueni.tech@gmail.com>
2024-05-18 11:01:45 +0900

    #8 作業：selected_team_idカラムの作成

---
+++ src/todo-gantt/database/migrations/2024_05_18_015349_add_selected_team_id_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('selected_team_id')->nullable()->constrained('teams')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['selected_team_id']);
+            $table->dropColumn('selected_team_id');
+        });
+    }
+};
