1218cd0be17fa9cf8a50726a2d5af9ec205afe32
uenishi <ueni.tech@gmail.com>
2024-06-21 12:03:03 +0900

    #14 feat: updateYaskでend_dateがstart_dateより前の日付のエラーを修正する

--- src/todo-gantt/tests/Feature/TaskTest.php
+++ src/todo-gantt/tests/Feature/TaskTest.php
@@ -173,6 +173,51 @@ class TaskTest extends TestCase
     /**
      * @test
      */
+    public function updateTaskでend_dateがstart_date以前の場合のテスト()
+    {
+        $this->expectException(ValidationException::class);
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $team = Team::factory()->create();
+        $team->users()->attach($user);
+        $user->selected_team_id = $team->id;
+
+        $project = Project::createProject($user, $team, 'プロジェクト');
+
+        $task = Task::createTask($project, 'タスク', 'メモ', '2021-01-01', '2021-01-31');
+
+        $name = 'タスク2';
+        $note = 'メモ2';
+        $start_date = '2024-01-31';
+        $end_date = '2024-01-30';
+
+        // ValidationExceptionがスローされるため、ここでタスクは更新されない
+        $task = Task::updateTask($task, $name, $note, $start_date, $end_date);
+
+        // バリデーションエラーがスローされるため、この部分のコードは実行されない
+        $this->assertDatabaseMissing('tasks', [
+            'project_id' => $project->id,
+            'name' => $name,
+            'note' => $note,
+            'start_date' => $start_date,
+            'end_date' => $end_date,
+            'completed' => 0,
+        ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'project_id' => $project->id,
+            'name' => 'タスク',
+            'note' => 'メモ',
+            'start_date' => '2021-01-01',
+            'end_date' => '2021-01-31',
+            'completed' => 0,
+        ]);
+    }
+
+    /**
+     * @test
+     */
     public function 更新でend_dateがstart_dateより前の日付のテスト()
     {
         $user = User::factory()->create();
