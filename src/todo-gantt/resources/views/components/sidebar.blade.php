8a974861bca4525d54c912efee7a3f90a66b917e
uenishi <ueni.tech@gmail.com>
2024-06-24 11:25:41 +0900

    #18 feat: Improve sidebar team icons and styling

--- src/todo-gantt/resources/views/components/sidebar.blade.php
+++ src/todo-gantt/resources/views/components/sidebar.blade.php
@@ -3,7 +3,7 @@
     <div class="flex flex-col justify-start items-center gap-4">
       @foreach($teams as $team)
       @if($team->id == auth()->user()->selected_team_id)
-      <a href="{{route('teams.change', $team)}}" class="w-3/4 rounded-lg overflow-hidden border-2 p-[2px] border-white">
+      <a href="{{route('teams.change', $team)}}" class="w-12 h-12 flex justify-center items-center bg-gray-400 rounded-lg overflow-hidden border-2 p-[2px] border-white">
         @if(!$team->image_name)
         <img src="https://ui-avatars.com/api/?name={{$team->name}}&background=999&color=eee&bold=true&length=1" alt="">
         @else
@@ -11,7 +11,7 @@
         @endif
       </a>
       @else
-      <a href="{{route('teams.change', $team)}}" class="w-3/4 rounded-lg overflow-hidden">
+      <a href="{{route('teams.change', $team)}}" class="w-12 h-12 flex justify-center items-center bg-gray-400 rounded-lg overflow-hidden">
         @if(!$team->image_name)
         <img src="https://ui-avatars.com/api/?name={{$team->name}}&background=999&color=eee&bold=true&length=1" alt="">
         @else
@@ -20,7 +20,7 @@
       </a>
       @endif
       @endforeach
-      <button @click="toggleTeamStoreModal()" class="w-3/4 aspect-square bg-gray-500 rounded-lg overflow-hidden border-2 border-gray-500">
+      <button @click="toggleTeamStoreModal()" class="w-12 h-12 aspect-square bg-gray-500 rounded-lg overflow-hidden border-2 border-gray-500">
         <span class="text-white text-3xl flex justify-center items-center w-full h-full">
           <i class="fa-solid fa-plus"></i>
         </span>
