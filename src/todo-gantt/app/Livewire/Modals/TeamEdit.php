f4f0018feb8dbf816d51a04c143383b29bc93f52
uenishi <ueni.tech@gmail.com>
2024-07-07 10:52:44 +0900

    #20 feat: Remove user from team and update current team if necessary

--- src/todo-gantt/app/Livewire/Modals/TeamEdit.php
+++ src/todo-gantt/app/Livewire/Modals/TeamEdit.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Modals;
 
+use App\Models\User;
 use Livewire\Component;
 
 class TeamEdit extends Component
@@ -49,6 +50,23 @@ class TeamEdit extends Component
     $this->isTeamNameChanged = false;
     $this->resetErrorBag();
     $this->resetValidation();
+  }
+
+  public function removeUserFromTeam($user_id)
+  {
+    $this->selectedTeam->users()->detach($user_id);
+
+    $user = User::find($user_id);
+    if ($user->teams->count() > 0) {
+      User::changeCurrentTeam($user, $user->teams->first());
+    } else {
+      $user->selected_team_id = null;
+      $user->save();
+    }
+
+    if($this->selectedTeam->users->count() === 0){
+      $this->selectedTeam->delete();
+    }
   }
 
   public function render()
--- src/todo-gantt/resources/views/livewire/modals/team-edit.blade.php
+++ src/todo-gantt/resources/views/livewire/modals/team-edit.blade.php
@@ -13,11 +13,21 @@
           @csrf
           @method('PUT')
           <div class="flex flex-col">
-            <div class="flex flex-col gap-4">
+            <div class="flex flex-col gap-2">
               <div class="flex flex-col gap-2">
                 <label for=" team-name">チーム名</label>
                 <input id="team-name" type="text" wire:model.live.debounce.150ms="team_name" name="team_name">
               </div>
+              @if($errors->any())
+              @foreach($errors->all() as $error)
+              <p class="text-red-500 text-xs">{{$error}}</p>
+              @endforeach
+              @endif
+              @if($isTeamNameChanged && $team_name && !$errors->any())
+              <button type="submit" class="bg-primary-500 text-white text-sm p-1 rounded self-end">更新</button>
+              @else
+              <button type="submit" class="bg-primary-500 text-white text-sm p-1 rounded self-end opacity-30" disabled>更新</button>
+              @endif
               <div>
                 <p>チームアイコン</p>
                 <div class="flex justify-start items-baseline gap-5 mt-2">
@@ -31,20 +41,24 @@
                   <a class="text-primary-700" href="{{route('upload-image.edit', $selectedTeam)}}">編集</a>
                 </div>
               </div>
-
+              <div>
+                <p>チームメンバー</p>
+                <div class="flex flex-col gap-2 mt-2 ">
+                  <div class="bg-white rounded p-2 border border-gray-500 h-20 overflow-y-auto">
+                    @foreach($selectedTeam->users as $user)
+                    <div class="flex justify-between items-center">
+                      <p>{{$user->name}}</p>
+                      @if($user->id !== auth()->user()->id)
+                      <button onclick="confirm('本当に削除しますか？')" wire:click="removeUserFromTeam({{$user->id}})" class="text-red-200 text-xs hover:text-red-500">削除</button>
+                      @else
+                      <button onclick="confirm('本当に退出しますか？')" wire:click="removeUserFromTeam({{$user->id}})" class="text-red-200 text-xs hover:text-red-500">退出</button>
+                      @endif
+                    </div>
+                    @endforeach
+                  </div>
+                </div>
+              </div>
             </div>
-            @if($errors->any())
-            @foreach($errors->all() as $error)
-            <p class="text-red-500 text-xs">{{$error}}</p>
-            @endforeach
-            @endif
-
-            @if($isTeamNameChanged && $team_name && !$errors->any())
-            <button type="submit" class="bg-primary-500 text-white text-sm mt-2 p-1 rounded self-end">更新</button>
-            @else
-            <button type="submit" class="bg-primary-500 text-white text-sm mt-2 p-1 rounded self-end opacity-30" disabled>更新</button>
-            @endif
-          </div>
         </form>
         <div class="mb-6"></div>
         <livewire:delete-team :selectedTeam="$selectedTeam" />
