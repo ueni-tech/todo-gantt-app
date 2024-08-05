99af289216a75ea8e6262c4071e65b85fafab46c
uenishi <ueni.tech@gmail.com>
2024-08-05 22:56:08 +0900

    #27 プロジェクトのレイアウトを更新

--- src/todo-gantt/resources/views/components/tasks.blade.php
+++ src/todo-gantt/resources/views/components/tasks.blade.php
@@ -1,33 +1,10 @@
-<div class="max-h-[calc(100vh-250px)] overflow-y-auto hidden-scrollbar hidden-scrollbar::-webkit-scrollbar">
+<div class="mt-2 max-h-[calc(100vh-280px)] overflow-y-auto hidden-scrollbar hidden-scrollbar::-webkit-scrollbar">
     <ul class="flex flex-col gap-3 hidden-scrollbar hidden-scrollbar::-webkit-scrollbar">
         @foreach($tasks as $task)
         <livewire:task :task="$task" :key="$task->id" />
         @endforeach
         <li>
-            <div class="flex justify-between items-baseline">
-                <livewire:store-task :project="$project" />
-                <div class="flex justify-end items-baseline gap-2">
-                    <form action="{{route('projects.update-status', $project)}}" method="POST" id="statusForm">
-                        @csrf
-                        @method('PATCH')
-                        <input type="hidden" name="status" value="{{$project->status_name == 'incomplete' ? 'completed' : 'incomplete'}}">
-                        <button type="submit" class="text-xs text-white bg-primary-500 hover:bg-primary-600 p-1 rounded-md shadow-md">
-                            {{$project->status_name == 'incomplete' ? '完了' : '未完'}}
-                        </button>
-                    </form>
-                    @if($project->status_name === 'incomplete')
-                    <form action="{{route('projects.update-status', $project)}}" method="POST" id="statusForm">
-                        @csrf
-                        @method('PATCH')
-                        <input type="hidden" name="status" value="pending">
-                        <button type="submit" class="text-xs text-white bg-gray-500 hover:bg-gray-600 p-1 rounded-md shadow-md">
-                            保留
-                        </button>
-                    </form>
-                    @endif
-                    <livewire:delete-project :project="$project" />
-                </div>
-            </div>
+            <livewire:store-task :project="$project" />
         </li>
     </ul>
 </div>
