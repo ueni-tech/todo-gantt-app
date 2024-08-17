a540d9c83151f2a8719f70bb0ef42f2ca9605b9c
uenishi <ueni.tech@gmail.com>
2024-08-17 13:01:20 +0900

    #40 イーガーロードでn+1解消

--- src/todo-gantt/app/Models/Gantt.php
+++ src/todo-gantt/app/Models/Gantt.php
@@ -14,7 +14,9 @@ class Gantt extends Model
     public static function getGanttData(User $user): array
     {
         $current_team = $user->selectedTeam;
-        $projects = $current_team->projects;
+        $projects = $current_team->projects()->with(['tasks' => function($query) {
+            $query->orderBy('start_date');
+        }, 'user'])->get();
 
         $ganttData = [];
 
@@ -28,7 +30,7 @@ class Gantt extends Model
 
     private static function processProject($project): array
     {
-        $tasks = $project->tasks->sortBy('start_date');
+        $tasks = $project->tasks;
 
         $projectData = [
             'id' => "project-{$project->id}",
@@ -54,9 +56,8 @@ class Gantt extends Model
     private static function processTasks(Collection $tasks, Project $project): Collection
     {
         $processedTasks = collect();
-        $previousTaskId = null;
 
-        foreach ($tasks as $index => $task) {
+        foreach ($tasks as $task) {
             $taskData = [
                 'id' => (string) $task->id,
                 'name' => $task->name,
@@ -64,8 +65,8 @@ class Gantt extends Model
                 'end' => $task->end_date,
                 'progress' => 0,
                 'custom_class' => "user-{$task->user_id}-task",
-                'user' => $project->user->name,
-                'user_id' => $task->user_id,
+                'user' => $project->name,
+                'user_id' => $project->user_id,
             ];
 
             $processedTasks->push($taskData);
