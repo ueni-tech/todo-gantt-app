e927e64f36abbca5f6381a1b6c2f8eca8f81ee62
uenishi <ueni.tech@gmail.com>
2024-09-16 11:10:36 +0900

    #47 コントローラーの整理

--- src/todo-gantt/app/Http/Controllers/TeamController.php
+++ src/todo-gantt/app/Http/Controllers/TeamController.php
@@ -5,28 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\TeamRequest;
 use App\Models\Team;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(TeamRequest $request)
     {
         $team = Team::createTeam($request->input('team_name'));
@@ -40,25 +21,6 @@ class TeamController extends Controller
         return redirect()->back();
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Team $team)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Team $team)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(TeamRequest $request, Team $team)
     {
         $team = Team::updateName($request->input('team_name'), $team);
@@ -67,9 +29,6 @@ class TeamController extends Controller
         return redirect()->back();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Team $team)
     {
         if ($team->image_name) {
@@ -86,9 +45,6 @@ class TeamController extends Controller
         return redirect()->route('index');
     }
 
-    /**
-     * チームを切り替える
-     */
     public function change(Team $team)
     {
         $user = User::find(auth()->id());
--- src/todo-gantt/app/Http/Controllers/UploadImageController.php
+++ src/todo-gantt/app/Http/Controllers/UploadImageController.php
@@ -5,14 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UploadImageRequest;
 use App\Models\Team;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class UploadImageController extends Controller
 {
-  /**
-   * Show the form for editing the specified resource.
-   */
   public function edit(string $id)
   {
     $user = User::with(['teams', 'selectedTeam'])->find(auth()->id());
@@ -22,9 +17,6 @@ class UploadImageController extends Controller
     return view('uploadImage', compact('teams', 'team'));
   }
 
-  /**
-   * Update the specified resource in storage.
-   */
   public function update(UploadImageRequest $request, string $id)
   {
     $imageData = $request->input('image_data');
